0aacda994da0a7546d5eebd6d16260be46e40308
Dmitriy Logachov <demonwork@yandex.ru>
2018-09-20 12:53:33 +0500

    Реализовано сохранение координат и журнала.

--- api/controllers/GpsTrackController.php
+++ api/controllers/GpsTrackController.php
@@ -19,6 +19,43 @@ class GpsTrackController extends BaseController
 
     public function actionCreate()
     {
-        return parent::createSimpleObjects();
+        $request = \Yii::$app->request;
+
+        // запись для загружаемого файла
+        $rawData = $request->getRawBody();
+        $items = json_decode($rawData, true);
+        $result = self::createSimpleObjects($items);
+
+        return $result;
+    }
+
+    protected function createSimpleObjects($items)
+    {
+        $success = true;
+        $saved = array();
+        foreach ($items as $item) {
+            $old_id = $item['_id'];
+            $line = self::createSimpleObject($item);
+            if ($line->save()) {
+                $saved[] = [
+                    '_id' => $old_id,
+                ];
+            } else {
+                $success = false;
+            }
+        }
+
+        return ['success' => $success, 'data' => $saved];
+    }
+
+    protected function createSimpleObject($item)
+    {
+        /** @var ActiveRecord $class */
+        /** @var ActiveRecord $line */
+        $class = $this->modelClass;
+        unset($item['_id']);
+        $line = new $class;
+        $line->setAttributes($item, false);
+        return $line;
     }
 }
